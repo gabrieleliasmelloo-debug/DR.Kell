9c58e45a7d12d116133d4c1c3a80e755f196d537
gabrieleliasmelloo-debug <gabrieleliasmelloo@gmail.com>
2026-09-14 18:54:28 -0300

    Publicar o aplicativo Dr. Kell

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "dr-kell-v1";
+const FILES = ["./", "./caloria.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
